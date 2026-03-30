709428cfc1598ecc5b6c851599af426e6598ffcc
BeniaminKowolPL <beniamin.kowol@wfirma.pl>
2026-03-30 10:21:34 +0200

    Fix console styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,24 @@
-document.querySelectorAll('div.highlighter-rouge').forEach(function(block) {
+document.querySelectorAll('pre').forEach(function(block) {
+  if(block.querySelector('.copy-btn')) return;
+  
   const btn = document.createElement('button');
   btn.className = 'btn btn-outline-light copy-btn';
   btn.innerText = 'Kopiuj';
+  
   btn.onclick = function() {
-    const codeBlock = block.querySelector('code');
-    navigator.clipboard.writeText(codeBlock.innerText);
+    const codeText = block.querySelector('code') ? block.querySelector('code').innerText : block.innerText;
+    navigator.clipboard.writeText(codeText.trim());
+    
     const originalText = btn.innerText;
     btn.innerText = 'Skopiowano!';
     btn.classList.replace('btn-outline-light', 'btn-success');
+    
     setTimeout(() => {
       btn.innerText = originalText;
       btn.classList.replace('btn-success', 'btn-outline-light');
     }, 2000);
   };
+  
   block.appendChild(btn);
 });
 
